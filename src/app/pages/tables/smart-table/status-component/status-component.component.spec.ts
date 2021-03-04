6c68df7d5defb80f7a2a70b75e4859baf60d5877
Toyeeb <toyeeb@flutterwavego.com>
2021-03-04 17:39:51 +0100

    init

---
+++ src/app/pages/tables/smart-table/status-component/status-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StatusComponentComponent } from './status-component.component';
+
+describe('StatusComponentComponent', () => {
+  let component: StatusComponentComponent;
+  let fixture: ComponentFixture<StatusComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StatusComponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StatusComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
